576fc2076c6237916ee2361d11bd150895cc0cd9
Devin Williams <devinwilliams@Devins-MacBook-Pro-2.local>
2020-02-02 20:07:56 -0500

    master

--- lib/Controller/SelectBarberController.dart
+++ lib/Controller/SelectBarberController.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:trimmz/Model/SuggestedBarbers.dart';
 import '../Model/ClientBarbers.dart';
 import '../globals.dart' as globals;
 import 'BookingController.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:stream_transform/stream_transform.dart';
+import 'dart:async';
+import '../calls.dart';
 
 class SelectBarberScreen extends StatefulWidget {
   final List<ClientBarbers> clientBarbers;
@@ -12,16 +17,37 @@ class SelectBarberScreen extends StatefulWidget {
 }
 
 class SelectBarberScreenState extends State<SelectBarberScreen> {
+  TextEditingController _search = new TextEditingController();
+  StreamController<String> searchStreamController = StreamController();
   List<ClientBarbers> clientBarbers = [];
+  List<SuggestedBarbers> searchBarbers = [];
 
   @override
   void initState() {
     super.initState();
-
     clientBarbers = widget.clientBarbers;
+
+    searchStreamController.stream
+    .debounce(Duration(milliseconds: 100))
+    .listen((s) => _searchValue(s));
   }
 
-  Widget _buildGridTile(ClientBarbers barber, int index, double scale) {
+  _searchValue(String string) async {
+    if(_search.text.length > 2) {
+      var res = await getSearchBarbers(context, _search.text);
+      print(res);
+      setState(() {
+        searchBarbers = res;
+      });
+    }
+    if(_search.text.length <= 2) {
+      setState(() {
+        searchBarbers = [];
+      });
+    }
+  }
+
+  Widget _buildGridTile(var barber, int index, double scale) {
     return new GestureDetector(
       onTap: () {},
       child: Container(
@@ -96,24 +122,71 @@ class SelectBarberScreenState extends State<SelectBarberScreen> {
   }
 
   buildBarberList(List<ClientBarbers> barbers) {
-    return new GridView.builder(
-      itemCount: barbers.length,
-      padding: EdgeInsets.all(2),
-      gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        mainAxisSpacing: 15.0,
-        crossAxisSpacing: 15.0,
-        childAspectRatio: 0.9
-      ),
-      itemBuilder: (context, index) {
-        return new Card(
-          child: _buildGridTile(barbers[index], index, 1.1),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(2.0))
+    if(clientBarbers.length > 0) {
+      return new GridView.builder(
+        itemCount: barbers.length,
+        padding: EdgeInsets.all(2),
+        gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 15.0,
+          crossAxisSpacing: 15.0,
+          childAspectRatio: 0.9
+        ),
+        itemBuilder: (context, index) {
+          return new Card(
+            child: _buildGridTile(barbers[index], index, 1.1),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(2.0))
+            ),
+          );
+        },
+      );
+    }else {
+      if(searchBarbers.length > 0) {
+        return new GridView.builder(
+          itemCount: searchBarbers.length,
+          padding: EdgeInsets.all(2),
+          gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 15.0,
+            crossAxisSpacing: 15.0,
+            childAspectRatio: 0.9
           ),
+          itemBuilder: (context, index) {
+            return new Card(
+              child: _buildGridTile(searchBarbers[index], index, 1.1),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(2.0))
+              ),
+            );
+          },
         );
-      },
-    );
+      }else {
+        return new Container(
+          child: Column(
+            children: [
+              Expanded(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget> [
+                      Icon(LineIcons.search, size: MediaQuery.of(context).size.height * .2, color: Colors.grey[600]),
+                      Text(
+                        'Search a barber to book an apppointment.',
+                        style: TextStyle(
+                          fontSize: MediaQuery.of(context).size.height * .018,
+                          color: Colors.grey[600]
+                        )
+                      ),
+                    ]
+                  )
+                )
+              ),
+            ]
+          )
+        );
+      }
+    }
   }
 
   @override
@@ -130,11 +203,49 @@ class SelectBarberScreenState extends State<SelectBarberScreen> {
         backgroundColor: Colors.black,
         appBar: AppBar(
           title: Text("Select Barber"),
+          bottom: clientBarbers.length > 0 ? PreferredSize(preferredSize: const Size.fromHeight(0.0), child: Container()) : PreferredSize(
+            preferredSize: Size.fromHeight(MediaQuery.of(context).size.height * .045),
+            child: Theme(
+              data: Theme.of(context).copyWith(accentColor: Colors.white),
+              child: Container(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                height: MediaQuery.of(context).size.height * .045,
+                alignment: Alignment.center,
+                child: TextField(
+                  autofocus: false,
+                  controller: _search,
+                  onChanged: (val) {
+                    searchStreamController.add(val);
+                  },
+                  autocorrect: false,
+                  textInputAction: TextInputAction.done, 
+                  decoration: new InputDecoration(
+                    contentPadding: EdgeInsets.all(8.0),
+                    hintText: 'Search',
+                    fillColor: globals.darkModeEnabled ? Colors.grey[900] : Colors.grey[100],
+                    filled: true,
+                    hintStyle: TextStyle(
+                      color: globals.darkModeEnabled ? Colors.white : Colors.black
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide.none
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide.none
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
-        body: new Container(
-          width: MediaQuery.of(context).size.width,
-          child: buildBarberList(clientBarbers)
-        ),
+        body: new Stack(
+          children: <Widget>[
+            buildBarberList(clientBarbers)
+          ]
+        )
       )
     );
   }
